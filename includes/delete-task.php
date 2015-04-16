e844846c8907d3548bf8cbe99bcefb2b18b3fdd7
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-16 16:56:46 -0700

    video 14

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,1 +1,9 @@
-delete-task.php
+<?php
+	$task_id = strip_tags($POST['id']);
+	require('connect.php');
+	$mysqli = new mysqli('localhost', 'root', 'root', 'task');
+
+	if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+	 	# code...
+	 } 
+?>
