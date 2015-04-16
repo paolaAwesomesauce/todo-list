b33b5f29edef8cad05615b9572df08596949f8a4
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-04-16 11:04:23 -0700

    video 9

--- index.php
+++ index.php
@@ -16,4 +16,22 @@
 	</form>
 	</div>
 </body>
+<script type="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+//calling the and task function 
+	add_task(); 
+
+	function add_task(){
+		$('.add-new-task').submit(function(){
+			var new_task = $('add-new-task input[name-task]').val();
+			if (new_task != '') {
+				$.post('includes/add-task.php', { task: new_task}, function(delta){
+					$(('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+				})
+			}
+			return false;
+		});
+	}
+</script>
 </html>
